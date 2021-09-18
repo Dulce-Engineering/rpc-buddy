19447073b6a777d9cedb6a5e9b8834f70946c1c6
Esteban Ramirez <netssrmrz@yahoo.com.au>
2021-09-18 13:53:41 +1000

    attach frontend

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const RPC_Buddy = require('./RPC_Buddy');
+
 const app = express();
 const port = 80;
 const rpc_buddy = new RPC_Buddy(app, '/rpc-server', '/rpc-client');
 
-app.get('/', Health);
-function Health(req, res)
-{
-  res.send('OK');
-}
+app.use(express.static('frontend'));
 
 app.listen(port, Listen);
 function Listen()
