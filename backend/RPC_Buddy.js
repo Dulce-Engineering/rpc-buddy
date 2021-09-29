142cc363d475ffca5a295afc9f9ac3ddd7113bb4
Esteban Ramirez <esteban.ramirez@playtravel.com.au>
2021-09-29 18:40:58 +1000

    support koa middleware

--- backend/RPC_Buddy.js
+++ backend/RPC_Buddy.js
@@ -1,47 +1,59 @@
+const Utils = require("../utils");
 
 class RPC_Buddy
 {
-  constructor(express_app, server_url, client_url, class_instances, fns, client_type, on_error_fn)
+  constructor(app, server_url, client_url, class_instances, fns, client_type, on_error_fn)
   {
     this.class_instances = class_instances;
     this.fns = fns;
     this.on_error_fn = on_error_fn;
-    this.Client_Express = this.Client_Express.bind(this);
-    this.Client_Koa = this.Client_Koa.bind(this);
-    this.Server_Express = this.Server_Express.bind(this);
-    this.Server_Koa = this.Server_Koa.bind(this);
+    this.server_url = server_url;
 
-    if (client_type == "koa")
+    const app_handler = new client_type(this, app);
+    app_handler.rpc_buddy = this;
+    this.Set_Routes(app_handler, server_url, client_url);
+  }
+
+  Set_Routes(app_handler, server_url, client_url)
+  {
+    app_handler.Set_Client_Route(client_url);
+
+    for (const fn of this.fns)
     {
-      express_app.post(server_url, this.Server_Koa);
-      express_app.get(client_url, this.Client_Koa);
-    }
-    else
-    {
-      express_app.post(server_url, this.Server_Express);
-      express_app.get(client_url, this.Client_Express);
+      const fn_url = this.Get_Fn_URL(server_url, fn);
+      const fn_middleware = fn.middleware;
+      app_handler.Set_Server_Route(fn_url, fn_middleware);
     }
   }
 
-  Client(req)
+  Client(server_host, class_name, noexport)
   {
     let class_def;
 
-    const server_host = req.query.serverHost || "";
-    const class_name = req.query.class;
     if (class_name)
     {
-      class_def = 
-        this.Client_Class(class_name, server_host) +
-        `export default ${class_name};`;
+      class_def = this.Client_Class(class_name, server_host);
+      if (noexport == undefined)
+      {
+        class_def += `export default ${class_name};`;
+      }
     }
     else
     {
       const class_defs = this.class_instances.map(c => this.Client_Class(c.name, server_host));
+      class_def = class_defs.join("");
+
       const class_names = this.class_instances.map(c => c.name);
-      class_def = 
-        class_defs.join("") +
-        `export default {${class_names.join(", ")}};`;
+      const class_names_str = class_names.join(", ");
+
+      if (noexport == undefined)
+      {
+        class_def += `export default {${class_names_str}};`;
+      }
+      else
+      {
+        class_def += `const rpc_classes = [${class_names_str}];`;
+      }
     }
 
     return class_def;
@@ -49,13 +61,14 @@ class RPC_Buddy
 
   Client_Class(class_name, server_host)
   {
-    //const class_instance = this.class_instances.find(c => c.name == class_name);
     const class_fns = this.fns.filter(fn => fn.name.split(".")[0] == class_name);
-    const fn_names = class_fns.map(fn => fn.name.split(".")[1]);
-    const fn_bodies = fn_names.map(fn_name => `
-      static ${fn_name}()
+    const fn_bodies = class_fns.map(fn => `
+      static ${fn.name.split(".")[1]}()
       {
-        return ${class_name}.Fetch_RPC("${class_name}.${fn_name}", arguments);
+        const method = "${fn.name}";
+        const server_host = ${class_name}.server_host || "${server_host}";
+        const url = server_host + "${this.Get_Fn_URL(this.server_url, fn)}";
+        return ${class_name}.Fetch_RPC(url, method, arguments);
       }
     `);
 
@@ -64,7 +77,7 @@ class RPC_Buddy
       {
         ${fn_bodies.join("\n")}
 
-        static async Fetch_RPC(method, method_params)
+        static async Fetch_RPC(url, method, method_params)
         {
           let params = {}, param_id = 1;
           for (const method_param of method_params)
@@ -80,8 +93,8 @@ class RPC_Buddy
             params,
             id: Date.now()
           };
-          const server_host = ${class_name}.server_host || "${server_host}";
-          const http_res = await fetch(server_host + "/rpc-server?fn=" + method, 
+
+          const options =
           {
             method: "post", 
             headers: 
@@ -89,8 +102,17 @@ class RPC_Buddy
               "Content-Type": "application/json"
             },
             body: JSON.stringify(body)
-          });
-          ${class_name}.status = http_res.status;
+          };
+          if (${class_name}.headers)
+          {
+            options.headers = {...options.headers, ...${class_name}.headers};
+          }
+          
+          const http_res = await fetch(url, options);
+          if (${class_name}.On_Fetch)
+          {
+            ${class_name}.On_Fetch(url, options, http_res);
+          }
           const http_json = await http_res.json();
 
           return http_json.result;
@@ -101,20 +123,18 @@ class RPC_Buddy
     return class_def;
   }
 
-  Client_Koa(ctx)
+  Get_Fn_URL(server_url, fn)
   {
-    const class_def = this.Client(ctx);
+    /*let res;
 
-    ctx.set("Content-Type", "text/javascript");
-    ctx.body = class_def;
-  }
+    res = server_url;
+    if (!Utils.isEmpty(fn.middleware))
+    {
+      res += "/" + fn.name;
+    }
 
-  Client_Express(req, res)
-  {
-    const class_def = this.Client(req);
-
-    res.set("Content-Type", "text/javascript");
-    res.send(class_def);
+    return res;*/
+    return server_url + "/" + fn.name;
   }
 
   async Server(req_rpc)
@@ -146,36 +166,6 @@ class RPC_Buddy
     };
     
     return res_rpc;
-  }
-
-  async Server_Express(req, res, next)
-  {
-    const res_rpc = await this.Server(req.body);
-    res.json(res_rpc);
-
-    if (this.error)
-    {
-      if (this.on_error_fn)
-      {
-        this.on_error_fn(this.error, req, res, next);
-      }
-      this.error = null;
-    }
-  }
-
-  async Server_Koa(ctx)
-  {
-    const res_rpc = await this.Server(ctx.request.body);
-    ctx.body = res_rpc;
-
-    if (this.error)
-    {
-      if (this.on_error_fn)
-      {
-        await this.on_error_fn(this.error, ctx);
-      }
-      this.error = null;
-    }
   }
 
   Get_Fn(fn_namespace)
@@ -215,5 +205,112 @@ class RPC_Buddy
     return res;
   }
 }
+
+class Koa
+{
+  constructor(rpc_buddy, app)
+  {
+    this.app = app;
+    this.rpc_buddy = rpc_buddy;
+    this.Get_Client = this.Get_Client.bind(this);
+    this.Post_Server = this.Post_Server.bind(this);
+  }
+
+  Set_Client_Route(client_url)
+  {
+    this.app.get(client_url, this.Get_Client);
+  }
+
+  Set_Server_Route(fn_url, fn_middleware)
+  {
+    if (!Utils.isEmpty(fn_middleware))
+    {
+      this.app.post(fn_url, ...fn_middleware, this.Post_Server);
+    }
+    else
+    {
+      this.app.post(fn_url, this.Post_Server);
+    }
+  }
+
+  Get_Client(ctx)
+  {
+    const class_def = this.rpc_buddy.Client
+    (
+      ctx.query.serverHost, 
+      ctx.query.class, 
+      ctx.query.noexport
+    );
+
+    ctx.set("Content-Type", "text/javascript");
+    ctx.body = class_def;
+  }
+
+  async Post_Server(ctx)
+  {
+    const res_rpc = await this.rpc_buddy.Server(ctx.request.body);
+    ctx.body = res_rpc;
+
+    if (this.rpc_buddy.error)
+    {
+      if (this.rpc_buddy.on_error_fn)
+      {
+        await this.rpc_buddy.on_error_fn(this.rpc_buddy.error, ctx);
+      }
+      this.rpc_buddy.error = null;
+    }
+  }
+}
+RPC_Buddy.Koa = Koa;
+
+class Express
+{
+  constructor(rpc_buddy, app)
+  {
+    this.app = app;
+    this.rpc_buddy = rpc_buddy;
+    this.Get_Client = this.Get_Client.bind(this);
+    this.Post_Server = this.Post_Server.bind(this);
+  }
+
+  Set_Client_Route(client_url)
+  {
+    this.app.get(client_url, this.Get_Client);
+  }
+
+  Set_Server_Route(fn_url, fn_middleware)
+  {
+    this.app.post(server_url, this.Post_Server);
+  }
+
+  Get_Client(req, res)
+  {
+    const class_def = this.rpc_buddy.Client
+    (
+      req.query.serverHost, 
+      req.query.class, 
+      req.query.noexport
+    );
+
+    res.set("Content-Type", "text/javascript");
+    res.send(class_def);
+  }
+
+  async Post_Server(req, res, next)
+  {
+    const res_rpc = await this.Server(req.body);
+    res.json(res_rpc);
+
+    if (this.rpc_buddy.error)
+    {
+      if (this.rpc_buddy.on_error_fn)
+      {
+        this.rpc_buddy.on_error_fn(this.rpc_buddy.error, req, res, next);
+      }
+      this.rpc_buddy.error = null;
+    }
+  }
+}
+RPC_Buddy.Express = Express;
 
 module.exports = RPC_Buddy;
